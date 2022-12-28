e6a16a1c3c47e3a26b9fb3e32a7d2e438c780a8c
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2022-12-28 23:50:45 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const inputEl = document.querySelector(".search-api")
+const buttonEl = document.querySelector(".button-input")
+console.log(inputEl);
+console.log(buttonEl);
+
+buttonEl.addEventListener("click", e => {
+    console.log(e);
+})
