3619f381ac3bf746ae4bb92faad567553e875484
Vol4enak <52600927+Vol4enak@users.noreply.github.com>
2022-12-29 22:58:59 +0000

    Deploying to gh-pages from @ Vol4enak/goit-js-hw-11@aeaf2b3c301866bf352c46ab8c31666e7de3c1b6 🚀

--- index.9da3c526.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".search-api"),o=document.querySelector(".button-input");console.log(e),console.log(o),o.addEventListener("click",(e=>{console.log(e)}));
-//# sourceMappingURL=index.9da3c526.js.map
